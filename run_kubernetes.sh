6bcdcd14c6fcbb8385f072b22fd110ee08cbd19a
JAYARAM AITA <aithajayaram@gmail.com>
2023-10-07 10:55:08 +0530

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # This is your Docker ID/path
-dockerpath="satishdockers/udacitydevops_p4:v1.0.0"
+dockerpath="aitha/udacitydevops_p4:v1.0.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
