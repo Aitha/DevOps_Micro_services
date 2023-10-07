4589bcf8f6fad01268579a1427b992843c5406a7
JAYARAM AITA <aithajayaram@gmail.com>
2023-10-07 11:09:09 +0530

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,13 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath="aitha/udacitydevops_p4:v1.0.0"
+dockerpath="aitha/udacitydevops:v1.0.0"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 docker login 
-docker image tag udacity_p4 $dockerpath
+docker image tag udacity $dockerpath
 
 # Step 3:
 # Push image to a docker repository
