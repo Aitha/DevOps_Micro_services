8b60220acfd11ac175f8410ab6cea11f0c79f94b
JAYARAM AITA <aithajayaram@gmail.com>
2023-10-07 11:08:26 +0530

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=udacity_p4 .
+docker build --tag=udacity .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 udacity_p4
+docker run -p 8000:80 udacity
